1931e3ae4a7710b07ac401e7fd21f9c7b76caa68
AndriiVMWork <andriimuzika@gmail.com>
2019-11-26 07:28:18 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var ap = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks!',
+    image: './assets/vmSocks-green-onWhite.jpg',
+    linka: 'https://google.com',
+    title: 'Title',
+    altText: 'A pair of socks.',
+	link:'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+  }
+})
