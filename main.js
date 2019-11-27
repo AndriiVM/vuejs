7cecac45bd8a9f6ec34f8b497a3e4200317ad07a
AndriiVMWork <andriimuzika@gmail.com>
2019-11-27 10:28:32 +0200

    Learn work with vue js. 6 lessons passed https://www.vuemastery.com/courses/intro-to-vue-js/class-&-style-binding/

--- main.js
+++ main.js
@@ -7,5 +7,34 @@ var ap = new Vue({
     title: 'Title',
     altText: 'A pair of socks.',
 	link:'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-  }
+      inStock: false,
+      details: ["80% cotton","20% polyester","Gender-neutral"],
+      variants:[
+          {
+              variantId:2234,
+              variantColor: "green",
+              variantImage: "./assets/vmSocks-green-onWhite.jpg"
+          },
+          {
+              variantId:2235,
+              variantColor: "blue",
+              variantImage: "./assets/vmSocks-blue-onWhite.jpg"
+          }
+      ],
+      cart: 0
+  },
+    methods:{
+        addToCart: function () {
+            this.cart  += 1
+        },
+        updateProduct: function (variantImage) {
+            this.image  = variantImage
+        },
+        dropFromCart: function () {
+            if(this.cart > 0)
+            {
+                this.cart  -= 1
+            }
+        },
+    }
 })
